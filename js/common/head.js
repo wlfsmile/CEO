2f6b6783846bea8fea98fa9df6bba13e8ebebfa7
wlfsmile <wlffsmile@gmail.com>
2017-10-11 13:06:15 +0800

    CEO

---
+++ js/common/head.js
@@ -0,0 +1,17 @@
+// html页面头部
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html lang='zh-CN'>");
+document.writeln("<meta charset='utf-8'>");
+document.writeln("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
+document.writeln("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+document.writeln("<head>");
+document.writeln("<title>CEO</title>");
+document.writeln(" <link href=\'../js/bootstrap/dist/css/bootstrap.min.css\'  rel=\"stylesheet\" type=\"text/css\" />");
+document.writeln("<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->\
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\
+    <!--[if lt IE 9]>\
+      <script src=\"https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js\"></script>\
+      <script src=\"https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js\"></script>\
+    <![endif]-->");
+document.writeln("</head>");
+document.writeln("<body >");
