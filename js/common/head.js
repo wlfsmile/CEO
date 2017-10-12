9f89a034338a3cca2cdd1b5caa7270f82c5a262e
wlfsmile <wlffsmile@gmail.com>
2017-10-12 22:50:14 +0800

    2017-10-12

--- js/common/head.js
+++ js/common/head.js
@@ -6,7 +6,9 @@ document.writeln("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
 document.writeln("<meta name='viewport' content='width=device-width, initial-scale=1'>");
 document.writeln("<head>");
 document.writeln("<title>CEO</title>");
-document.writeln(" <link href=\'../js/bootstrap/dist/css/bootstrap.min.css\'  rel=\"stylesheet\" type=\"text/css\" />");
+document.writeln("<link href=\'../js/bootstrap/dist/css/bootstrap.css\'  rel=\"stylesheet\" type=\"text/css\" />");
+//base.css要放在bootstrap.css后面
+document.writeln("<link href=\'../css/base/base.css\'  rel=\"stylesheet\" type=\"text/css\" />");
 document.writeln("<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->\
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\
     <!--[if lt IE 9]>\
