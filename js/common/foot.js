0904ec9c503b145cbdcd92af3a5fe7ad2b2681b1
wlfsmile <wlffsmile@gmail.com>
2017-10-14 22:47:36 +0800

    2017-10-14

--- js/common/foot.js
+++ js/common/foot.js
@@ -1,5 +1,4 @@
 // html页面尾部
-document.writeln("<script src=\"../js/jquery-1.11.3.min.js\"></script>");
 document.writeln("<script src=\"../js/bootstrap/dist/js/bootstrap.min.js\"></script>");
 document.writeln("</body>");
 document.writeln("</html>");
--- js/common/head.js
+++ js/common/head.js
@@ -1,10 +1,11 @@
 // html页面头部
 document.writeln("<!DOCTYPE html>");
-document.writeln("<html lang='zh-CN'>");
-document.writeln("<meta charset='utf-8'>");
-document.writeln("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
-document.writeln("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+document.writeln("<html lang=\'zh-CN\'>");
 document.writeln("<head>");
+document.writeln("<meta charset=\'utf-8\'>");
+document.writeln("<meta name=\'viewport,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\'>");
+document.writeln("<meta http-equiv=\'X-UA-Compatible\' content=\'IE=edge\'>");
+document.writeln("<meta http-equiv=\'X-UA-Compatible\' content=\'IE=9\' />");
 document.writeln("<title>CEO</title>");
 document.writeln("<link href=\'../js/bootstrap/dist/css/bootstrap.css\'  rel=\"stylesheet\" type=\"text/css\" />");
 //base.css要放在bootstrap.css后面
@@ -12,8 +13,9 @@ document.writeln("<link href=\'../css/base/base.css\'  rel=\"stylesheet\" type=\
 document.writeln("<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->\
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\
     <!--[if lt IE 9]>\
-      <script src=\"https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js\"></script>\
-      <script src=\"https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js\"></script>\
+      <script src=\'https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js\'></script>\
+      <script src=\'https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js\'></script>\
     <![endif]-->");
+document.writeln("<script src=\'../js/jquery-1.11.3.min.js\'></script>");
 document.writeln("</head>");
 document.writeln("<body >");
